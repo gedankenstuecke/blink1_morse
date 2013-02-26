8c1cfc5de2ee287a5193ef84d22808ec1aa228de
Bastian Greshake <bgreshake@googlemail.com>
2013-02-26 23:23:51 +0100

    here we go

---
+++ README.md
@@ -0,0 +1,35 @@
+# blink1-tool morse code wrapper
+This is a small python script that wraps around the blink1-tool for the command line. It translates the command line input text into [morse code](http://en.wikipedia.org/wiki/Morse_code) and emits them through the blink1. 
+
+## Setup
+* Make sure you have [downloaded the right version of the blink1-tool](http://thingm.com/products/blink-1.html) for your OS. 
+* Set the BLINK_TOOL_PATH in morse.py to the full path to your blink1-tool
+* That should be it! 
+
+## Examples
+Here are two short clips showing it in action: 
+* http://vine.co/v/b6wwHZJJUAQ
+* http://vine.co/v/b6ZtaiFzQWF
+
+## Gosh, this code is ugly!
+Yup, it is. It's a quick hack with some features added as an afterthought. But it seems to work. If you want to rewrite it or make it less ugly: Go for it!
+
+## Commands
+```
+usage: blink1.py [-h] [-c COLOR] [-s] [-tc TARGET_COLOR] [-t TIME] text
+
+positional arguments:
+  text                  This is the text that should be emitted
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c COLOR, --color COLOR
+                        The (initial) color of the blinks. Specify as R,G,B.
+                        Default: 255,255,255
+  -s, --shift           Should the color shift? If yes: --target-color needs
+                        to be specified
+  -tc TARGET_COLOR, --target-color TARGET_COLOR
+                        The final color of the blinks. Specify as R,G,B.
+                        Default: 255,255,255
+  -t TIME, --time TIME  Duration of a single dit in milliseconds. Default: 100 
+```
---
+++ morse.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+import time, sys, os, subprocess, argparse
+
+# SET TO BLINK TOOL PATH
+BLINK_TOOL_PATH = "blink1-tool"
+
+morsetab = {
+        'A': '.-',              'a': '.-',
+        'B': '-...',            'b': '-...',
+        'C': '-.-.',            'c': '-.-.',
+        'D': '-..',             'd': '-..',
+        'E': '.',               'e': '.',
+        'F': '..-.',            'f': '..-.',
+        'G': '--.',             'g': '--.',
+        'H': '....',            'h': '....',
+        'I': '..',              'i': '..',
+        'J': '.---',            'j': '.---',
+        'K': '-.-',             'k': '-.-',
+        'L': '.-..',            'l': '.-..',
+        'M': '--',              'm': '--',
+        'N': '-.',              'n': '-.',
+        'O': '---',             'o': '---',
+        'P': '.--.',            'p': '.--.',
+        'Q': '--.-',            'q': '--.-',
+        'R': '.-.',             'r': '.-.',
+        'S': '...',             's': '...',
+        'T': '-',               't': '-',
+        'U': '..-',             'u': '..-',
+        'V': '...-',            'v': '...-',
+        'W': '.--',             'w': '.--',
+        'X': '-..-',            'x': '-..-',
+        'Y': '-.--',            'y': '-.--',
+        'Z': '--..',            'z': '--..',
+        '0': '-----',           ',': '--..--',
+        '1': '.----',           '.': '.-.-.-',
+        '2': '..---',           '?': '..--..',
+        '3': '...--',           ';': '-.-.-.',
+        '4': '....-',           ':': '---...',
+        '5': '.....',           "'": '.----.',
+        '6': '-....',           '-': '-....-',
+        '7': '--...',           '/': '-..-.',
+        '8': '---..',           '(': '-.--.-',
+        '9': '----.',           ')': '-.--.-',
+        ' ': ' ',               '_': '..--.-',
+}
+
+def translateText(ascii_text):
+    '''
+    Translate ASCII input to morse code
+    Uses the translation table above
+    not included characters are ignored
+    '''
+    morse_output = []
+    for c in ascii_text:
+        if morsetab.has_key(c):
+            morse_output.append(morsetab[c])
+    return morse_output
+
+def translateMorse(morse_array,base_length=100,color="255,255,255",shift_color=False,target_color="255,255,255"):
+    '''
+    Send the morse code out to the blink(1). Accepts up to 5 arguments:
+    1. morse_array: Text translated to morse by translateText() method
+    2. base_length: The base duration for a dit in milliseconds. All other lengths are calculated by this base length
+    3. color: The color the morse code should have. If shifting colors are used this is the starting color
+    4. shift_color: True/False, if True tries to shift the color over all blinks to target_color
+    5. Only used if shift_color = True, this color should (more or less) be reached at the last blnk
+    '''
+    base_color = splitColor(color)
+    target = splitColor(target_color)
+    number_blinks = numberBlinks(morse_array)
+    mod_r, mod_g, mod_b = colorDiff(base_color,target,number_blinks)
+    blink_counter = 0
+    for character in morse_array:
+        if character == " ":
+            waitEmit(base_length*4)
+        else:
+            for j in character:
+                if shift_color == True:
+                    color = newColor(color,[mod_r,mod_g,mod_b],target,blink_counter)
+                if j == "-":
+                    emitDit(base_length,color,dah=True)
+                elif j == ".":
+                    emitDit(base_length,color)
+                blink_counter += 1
+            waitEmit(base_length*2)
+
+def newColor(color,modulos,target,blink_counter):
+    color_array = splitColor(color)
+    out_color = []
+    for i,single_color in enumerate(color_array):
+        if color_array[i] < target[i]:
+            out_color.append(color_array[i] +  abs(modulos[i]))
+        elif color_array[i] > target[i]:
+            out_color.append(color_array[i] - abs(modulos[i]))
+        else:
+            out_color.append(color_array[i])
+    colors = ",".join([str(i) for i in out_color])
+    return colors    
+
+def splitColor(color):
+    '''
+    Take color input in blink(1)-tool-format and 
+    return as list with each value as integer 
+    '''
+    colors = [ int(i) for i in color.split(",") ]
+    return colors
+
+def colorDiff(base,target,number_blinks):
+    '''
+    Takes start color, end color and number of total blinks in text. 
+    Returns how much each color value has to be shifted in a given blink 
+    '''
+    color_difference = []
+    for i,value in enumerate(target):
+        diff = (value-base[i])/number_blinks
+        if diff == 0:
+            diff = 1
+        color_difference.append(diff)
+    return color_difference[0], color_difference[1], color_difference[2]
+    
+
+def numberBlinks(morse_array):
+    '''
+    Calculate how many times the blink(1) will have to emit a blink.
+    Needed to calculate the color shifting frequency
+    '''
+    length = 0
+    for i in morse_array:
+        if i != " ":
+            length += len(i)
+    return length
+            
+def waitEmit(length):
+    '''
+    We don't want to send a signal, just wait to be in line with morse specs
+    '''
+    print "wait " + str(length)
+    time.sleep(float(length)/1000)
+
+def emitDit(dit_length,color,dah=False):
+    '''
+    Lights please! Either a dit or a dah (3*time of dit)
+    '''
+    if dah==True:
+        command = BLINK_TOOL_PATH + " --rgb "+color+" --blink 1 -m 0 -t "+str(dit_length*3)
+    else:
+        command = BLINK_TOOL_PATH + " --rgb "+color+" --blink 1 -m 0 -t "+str(dit_length)
+    print command
+    output = subprocess.call(command, stdout=subprocess.PIPE, shell=True)
+    time.sleep(float(dit_length)/1000)
+
+def main():
+    '''
+    Parse arguments & run stuff if run from script
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text",type=str,help="This is the text that should be emitted")
+    parser.add_argument("-c","--color",type=str,default="255,255,255",help="The (initial) color of the blinks. Specify as R,G,B. Default: 255,255,255")
+    parser.add_argument("-s","--shift",action="store_true",help="Should the color shift? If yes: --target-color needs to be specified")
+    parser.add_argument("-tc","--target-color",type=str,default="255,255,255", help="The final color of the blinks. Specify as R,G,B. Default: 255,255,255")
+    parser.add_argument("-t","--time", type=int, default=100, help="Duration of a single dit in milliseconds. Default: 100") 
+    args = parser.parse_args()
+    print args
+    morse_text = translateText(args.text)
+    translateMorse(morse_text,base_length=args.time,color=args.color,shift_color=args.shift,target_color=args.target_color)
+
+if __name__ == "__main__":
+    main()
